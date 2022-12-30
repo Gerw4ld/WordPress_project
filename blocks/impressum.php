e9f48f81c6a61630fc0287553d3ceba4510b29ff
Gerwald Gindra-Vady <gerwald.gindra-vady@edu.fh-joanneum.at>
2022-12-30 11:21:23 +0100

    working on custom theme - impressum onclick show text

--- blocks/impressum.php
+++ blocks/impressum.php
@@ -1,14 +1,23 @@
+<link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>">
+
 <?php $impressum = get_field('impressum'); ?>
 
-    <div class="m-5">
+<div class="m-5">
+
         <?php
         if (!empty($impressum['title'])): ?>
-            <p><?php echo $impressum['title']; ?></p>
+            <input  type="checkbox" id="impressumToggle">
+            <label for="impressumToggle" style="cursor: pointer">
+                <p style="padding-right: 8px; margin: auto"><?php echo $impressum['title']; ?></p>
+            </label>
         <?php endif; ?>
-    </div>
-    <div class="m-2 px-3">
+        <i id="arrow" class="arrow down"></i>
+
+
+    <div class="m-2 px-3" id="impressumText">
         <?php
         if (!empty($impressum['text'])): ?>
             <p><?php echo $impressum['text']; ?></p>
         <?php endif; ?>
     </div>
+</div>
