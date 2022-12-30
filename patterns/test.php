e38cb1b49dc3641c69a4f9758ff2afbd209553bb
Gerwald Gindra-Vady <gerwald.gindra-vady@edu.fh-joanneum.at>
2022-12-30 14:58:19 +0100

    working on custom theme - added patterns dir and test.php pattern

---
+++ patterns/test.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Title: Test Footer
+ * Slug: patterns/test
+ * Categories: test-footer
+ * Viewport Width: 1280
+ * Block Types: core/parts/footer
+ * Inserter: yes
+ */
+?>
+<!-- wp:group {"align":"full","style":{"elements":{"link":{"color":{"text":"var:preset|color|foreground"}}},"spacing":{"padding":{"top":"35px","bottom":"30px"}}},"backgroundColor":"background-header","textColor":"foreground","className":"has-foreground","layout":{"inherit":true}} -->
+<div class="wp-block-group alignfull has-foreground has-foreground-color has-background-header-background-color has-text-color has-background has-link-color" style="padding-top:35px;padding-bottom:30px"><!-- wp:paragraph {"align":"center","fontSize":"small"} -->
+    <p class="has-text-align-center has-small-font-size">Powered by WordPress | TT2 Gopher, a modified TT2 theme just for test purposes</p>
+    <!-- /wp:paragraph --></div>
+<!-- /wp:group -->
